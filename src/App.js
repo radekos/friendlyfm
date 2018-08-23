5e5d7021add1d22c0c75e458502a66b1c6f9aae5
Radoslaw Kosminski <radoslawkosminski@hotmail.co.uk>
2018-08-23 17:48:25 +0100

    Implemented the listing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,126 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      apiKey: "fc05c9fdbe7159bbb7865e7a0758f727",
+      error: null,
+      isLoaded: false,
+      items: [null, null, null, null]
+    };
+  }
+
+  getData() {
+    fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=ikuzomi&api_key="+this.state.apiKey+"&format=json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.state.items[0] = result.recenttracks;
+          this.forceUpdate();
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+
+    fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=mazemagic&api_key=fc05c9fdbe7159bbb7865e7a0758f727&format=json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.state.items[1] = result.recenttracks;
+          this.forceUpdate();
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+
+    fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=roo_220&api_key=fc05c9fdbe7159bbb7865e7a0758f727&format=json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.state.items[2] = result.recenttracks;
+          this.forceUpdate();
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+
+    fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user=bkrpage94&api_key=fc05c9fdbe7159bbb7865e7a0758f727&format=json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.state.items[3] = result.recenttracks;
+          this.forceUpdate();
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  componentDidMount() {
+
+
+    this.interval = setInterval(() => this.getData(), 5000);
+
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const { error, isLoaded, items } = this.state;
+
+    if (error) 
+    {
+      return <div>ERROR: {error.message}</div>;
+    }
+    else
+    {
+      var isReady = true;
+      
+      items.map(item => {
+        if (item == null)
+        {
+          isReady = false;
+        }
+      });
+
+      if (isReady)
+      {
+        return (
+          <div>
+            {items.map(item => (
+              <div key={item['@attr'].user} className="line-item">
+                <span>{item['@attr'].user}</span> <span><img alt="" src={item.track[0].image[1]['#text']}/> {item.track[0].artist['#text']} - {item.track[0].name}</span> 
+              </div>
+            ))}
+          </div>
+        );
+      }
+      else
+      {
+        return <div>Loadingu...</div>;
+      }
+    }
   }
 }
 
